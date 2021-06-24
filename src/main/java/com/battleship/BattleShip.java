8dac4d1cda3555f381063fe430dbc96b75a7b5c9
GuitaristNSK <GuitaristNSK@yandex.ru>
2021-06-24 11:49:15 +0700

    Добавлено основное окно, поле игрока и слушатель

--- src/main/java/com/battleship/BattleShip.java
+++ src/main/java/com/battleship/BattleShip.java
@@ -1,7 +1,10 @@
 package com.battleship;
 
+import com.views.View;
+
 public class BattleShip {
     public static void main(String[] args) {
-
+        View view = new View();
+        view.init();
     }
 }
